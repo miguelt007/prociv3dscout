dd46c7940c492f7ac6e3e9f72dc59dc2509e044d
Miguel Teixeira <miguelt007@gmail.com>
2025-08-06 15:57:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def index():
         hover_data=["estadoocorrencia", "distrito", "concelho"],
         color="estado",
         zoom=6,
-        height="100%"
+        height=900
     )
     mapa.update_layout(mapbox_style="open-street-map")
     mapa_html = mapa.to_html(full_html=False)
